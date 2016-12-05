e707a63e35273338c3a2cc9bd90f795a44f6c208
Wenkai Qin <wqin@hmc.edu>
2016-12-05 14:49:08 -0800

    added pr1

---
+++ hw9pr1.m
@@ -0,0 +1,51 @@
+%% Part A
+close all
+load('E101_HW9.mat')
+
+Ts = 10/1000;
+Fs = 1/Ts;
+L = 1000;
+t = (0:L-1)*Ts;
+
+figure(1)
+plot(t(1:100),x(1:100))
+ylim([-10 10])
+title('Signal')
+xlabel('t (seconds)')
+ylabel('x(t)')
+
+X = 1/L*fft(x,L);
+P1 = abs(X(1:L/2));
+
+figure(2)
+f = Fs*(0:L/2-1)/L;
+plot(f, P1)
+title('DFT')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+N = 1000/106.5;
+%% Part B
+M = 500;
+n = -M:M-1;
+
+w=0.5*(1+cos(pi*n/M));
+xw = x(1:L).*w.';
+XW = 1/L*fft(xw);
+Pw = abs(XW(1:L/2));
+
+figure(3)
+plot(f, Pw)
+title('DFT')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+%% Part C
+A = max(x);
+a0 = sum(x)/size(x,1);
+d = a0/A;
+k = 1:4;
+
+ak = abs((A*sin(k*pi*d)) ./ (N*sin(k*pi/N)));
+
+kN = N*k;
